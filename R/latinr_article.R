b8340b9630350b6dbe375cfa0843cc607d188e79
Unknown <eliocampitelli@gmail.com>
2019-04-23 14:24:36 -0300

    Cambia keep_tex

--- R/latinr_article.R
+++ R/latinr_article.R
@@ -4,7 +4,7 @@
 #'
 #' @export
 latinr_article <- function(
-    ..., keep_tex = TRUE, highlight = "default", citation_package = "none", 
+    ..., keep_tex = FALSE, highlight = "default", citation_package = "none", 
     latex_engine = "xelatex"
 ) {
   pdf_document_format(
